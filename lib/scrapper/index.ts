65b2d63f45ebecf1ac7b34459bb1b3cc3663ae85
raphael <you@example.com>
2024-03-09 18:11:10 +0100

    test

--- lib/scrapper/index.ts
+++ lib/scrapper/index.ts
@@ -1,141 +1,158 @@
+"use server";
 import axios from "axios";
 import * as cheerio from "cheerio";
-import { extractCurrency } from "../utils";
-export async function scrapeAmazonProduct(productUrl: string) {
+import { extractCurrency, extractPrice } from "../utils";
+export async function scrapeAmazonProduct(productUrl: string, maxRetries = 3) {
   if (!productUrl) {
     console.log("Invalid product url");
     return;
   }
-
+  let retryCount = 0;
   //BrightData proxy configuration
-  const username = String(process.env.BRIGHT_DATA_USERNAME);
-  const password = String(process.env.BRIGHT_DATA_PASSWORD);
-  const port = 2225;
-  const session_id = (1000000 * Math.random()) | 0;
-  const options = {
-    auth: {
-      username: `${username}-session-${session_id}`,
-      password,
-    },
-    host: "brd.superproxy.io",
-    port,
-    rejectUnauthorized: false,
-  };
-  try {
-    //fetch the product page
-    const response = await axios.get(productUrl, options);
-    const $ = cheerio.load(response.data);
+  while (retryCount < maxRetries) {
+    try {
+      const username = String(process.env.BRIGHT_DATA_USERNAME);
+      const password = String(process.env.BRIGHT_DATA_PASSWORD);
+      const port = 2225;
+      const session_id = (1000000 * Math.random()) | 0;
+      const options = {
+        auth: {
+          username: `${username}-session-${session_id}`,
+          password,
+        },
+        host: "brd.superproxy.io",
+        port,
+        rejectUnauthorized: false,
+      };
+      //fetch the product page
+      const response = await axios.get(productUrl, options);
+      const $ = cheerio.load(response.data);
 
-    //Extract data
-    const title = $("#productTitle").text().trim();
-    const priceArr = $("#corePriceDisplay_desktop_feature_div div span")
-      .text()
-      .trim()
-      .split(/\s{4,}/)
-      .slice(0, 1)
-      .join("")
-      .split(" ")
-      .filter(
-        (num) => num !== "with" && num !== "percent" && num !== "savings"
-      );
-    // if (priceArr.length === 0 || priceArr[0] === "") {
-    //   console.log(priceArr);
-    //   return;
-    // }
-    const discountRate = priceArr[1] ? parseInt(priceArr[1]) : 0;
-    const currentPriceText = priceArr[0]
-      .split("")
-      .slice(1)
-      .filter((item) => item !== ",")
-      .join("");
+      //Extract data
+      const title = $("#productTitle").text().trim();
+      // console.log(title);
 
-    // Ensure currentPriceText is a valid number
-    const currentPrice = isNaN(parseFloat(currentPriceText))
-      ? 0 // Assign a default value if currentPriceText is not a valid number
-      : Number(parseFloat(currentPriceText).toFixed(2));
-
-    const originalPrice = (currentPrice / (1 - discountRate / 100)).toFixed(2);
-    const outOfStock =
-      $("#availability span").text().trim().toLowerCase() ===
-      "currently unavailable";
-    const images =
-      $("#imgBlkFront").attr("data-a-dynamic-image") ||
-      $("#landingImage").attr("data-a-dynamic-image") ||
-      "{}";
-    const currency = extractCurrency($(".a-price-symbol"));
-    const category = $("span.a-list-item a.a-link-normal.a-color-tertiary")
-      .text()
-      .trim()
-      .replace(/[\n+]/g, "")
-      .split(/\s{2,}/)
-      .filter((word) => word.trim() !== "")
-      .slice(-2)
-      .join("");
-    const reviewsCount = $("#acrCustomerReviewText")
-      .text()
-      .trim()
-      .replace(/[,]/g, "")
-      .split(" ")
-      .slice(0, 1)
-      .join("");
-    const starsFloat = parseFloat(
-      $('[data-hook="rating-out-of-text"]')
+      const priceArr = $(
+        "#corePriceDisplay_desktop_feature_div div.a-section span"
+      )
         .text()
         .trim()
-        .split(" ")
+        .split(/\s{4,}/)
         .slice(0, 1)
         .join("")
-    );
-    const stars = Math.round(starsFloat);
-    const imageUrls = Object.keys(JSON.parse(images));
-    const description = $("#feature-bullets")
-      .text()
-      .trim()
-      .replace(/[n]/g, "")
-      .split(/\s{2,}/)
-      .slice(1)
-      .filter((desc) => desc !== "Show more");
-    const descAlt = $("ul.a-unordered-list.a-vertical .a-list-item")
-      .text()
-      .trim()
-      .replace(/[\n+]/g, "")
-      .split(/\s{4,}/)
-      .slice(0, 1)
-      .join("")
-      .split(".");
-    const recommendationText = $("#histogramTable td.a-text-right.a-nowrap")
-      .text()
-      .trim()
-      .split("%")[0];
-    const recommendations = parseInt(recommendationText);
-    //Construct data object with scrapped information
-    const data = {
-      productUrl,
-      currency: currency || "$",
-      image: imageUrls[0],
-      title,
-      currentPrice: Number(currentPrice),
-      originalPrice: Number(originalPrice),
-      priceHistory: [],
-      discountRate: Number(discountRate),
-      category,
-      reviewsCount: Number(reviewsCount),
-      stars: Number(stars) || 0,
-      isOutOfStock: outOfStock,
-      description:
-        description.length > 0
-          ? description
-          : descAlt.length > 0
-          ? descAlt
-          : ["Check website for Product Details"],
-      lowestPrice: Number(currentPrice),
-      highestPrice: Number(originalPrice),
-      averagePrice: Number(currentPrice) || Number(originalPrice),
-      recommendations,
-    };
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.log("Failed to scrape from bright data", error);
+        .split(" ")
+        .filter(
+          (num) => num !== "with" && num !== "percent" && num !== "savings"
+        );
+      const text1 = extractPrice(
+        $("span.a-price-whole"),
+        $(".a.size.base.a-color-price"),
+        $(".a-button-selected .a-color-base")
+      );
+      console.log({ text1 });
+
+      if (priceArr.length === 0 || priceArr[0] === "") {
+        return;
+      }
+      const discountRate = priceArr[1] ? parseInt(priceArr[1]) : 0;
+      const currentPriceText = priceArr[0]
+        .split("")
+        .slice(1)
+        .filter((item) => item !== ",")
+        .join("");
+
+      // Ensure currentPriceText is a valid number
+      const currentPrice = isNaN(parseFloat(currentPriceText))
+        ? 0 // Assign a default value if currentPriceText is not a valid number
+        : Number(parseFloat(currentPriceText).toFixed(2));
+
+      const originalPrice = (currentPrice / (1 - discountRate / 100)).toFixed(
+        2
+      );
+      const outOfStock =
+        $("#availability span").text().trim().toLowerCase() ===
+        "currently unavailable";
+      const images =
+        $("#imgBlkFront").attr("data-a-dynamic-image") ||
+        $("#landingImage").attr("data-a-dynamic-image") ||
+        "{}";
+      const currency = extractCurrency($(".a-price-symbol"));
+      const category = $("span.a-list-item a.a-link-normal.a-color-tertiary")
+        .text()
+        .trim()
+        .replace(/[\n+]/g, "")
+        .split(/\s{2,}/)
+        .filter((word) => word.trim() !== "")
+        .slice(-2)
+        .join("");
+      const reviewsCount = $("#acrCustomerReviewText")
+        .text()
+        .trim()
+        .replace(/[,]/g, "")
+        .split(" ")
+        .slice(0, 1)
+        .join("");
+      const starsFloat = parseFloat(
+        $('[data-hook="rating-out-of-text"]')
+          .text()
+          .trim()
+          .split(" ")
+          .slice(0, 1)
+          .join("")
+      );
+      const stars = Math.round(starsFloat);
+      const imageUrls = Object.keys(JSON.parse(images));
+      const description = $("#feature-bullets")
+        .text()
+        .trim()
+        .replace(/[n]/g, "")
+        .split(/\s{2,}/)
+        .slice(1)
+        .filter((desc) => desc !== "Show more");
+      const descAlt = $("ul.a-unordered-list.a-vertical .a-list-item")
+        .text()
+        .trim()
+        .replace(/[\n+]/g, "")
+        .split(/\s{4,}/)
+        .slice(0, 1)
+        .join("")
+        .split(".");
+      const recommendationText = $("#histogramTable td.a-text-right.a-nowrap")
+        .text()
+        .trim()
+        .split("%")[0];
+      const recommendations = parseInt(recommendationText);
+      //Construct data object with scrapped information
+      const data = {
+        productUrl,
+        currency: currency || "$",
+        image: imageUrls[0],
+        title,
+        currentPrice: Number(currentPrice),
+        originalPrice: Number(originalPrice),
+        priceHistory: [],
+        discountRate: Number(discountRate),
+        category,
+        reviewsCount: Number(reviewsCount),
+        stars: Number(stars) || 0,
+        isOutOfStock: outOfStock,
+        description:
+          description.length > 0
+            ? description
+            : descAlt.length > 0
+            ? descAlt
+            : ["Check website for Product Details"],
+        lowestPrice: Number(currentPrice),
+        highestPrice: Number(originalPrice),
+        averagePrice: Number(currentPrice) || Number(originalPrice),
+        recommendations,
+        priceArr,
+      };
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.log("Failed to scrape from bright data", error);
+      retryCount++;
+    }
   }
 }
